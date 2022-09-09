2ab2cb4b5e7a8412956502a173ba55c784a12812
buntennn <bunten2005@gmail.com>
2022-09-09 00:44:32 -0500

    change swing mixin

--- src/main/java/com/ninni/decibel/mixin/LivingEntityMixin.java
+++ src/main/java/com/ninni/decibel/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package com.ninni.decibel.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,6 +11,7 @@ import com.ninni.decibel.sound.ItemSoundModifications;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.HitResult;
@@ -18,6 +20,9 @@ import net.minecraft.util.hit.HitResult;
 public abstract class LivingEntityMixin {
 
     private final LivingEntity mob = (LivingEntity) (Object) this;
+
+    @Shadow
+    protected abstract int getHandSwingDuration();
 
     // dunno if this works in multiplayer atm, needs to be tested
     @Inject(at = @At("TAIL"), method = "setCurrentHand")
@@ -35,13 +40,16 @@ public abstract class LivingEntityMixin {
         }
     }
 
-    @Inject(at = @At("TAIL"), method = "swingHand", cancellable = true)
-    public void addSwingSound(Hand hand, CallbackInfo info) {
-        HitResult result = mob.raycast(5, 0, false);
-        if (result.getType() != HitResult.Type.BLOCK) {
-            ItemStack stack = mob.getStackInHand(hand);
-            if (ItemSoundModifications.SWINGING_MAP.containsKey(stack.getItem())) {
-                mob.playSound(ItemSoundModifications.SWINGING_MAP.get(stack.getItem()).get(stack), 1, 1);
+    @Inject(at = @At("TAIL"), method = "Lnet/minecraft/entity/LivingEntity;swingHand(Lnet/minecraft/util/Hand;Z)V", cancellable = true)
+    public void addSwingSound(Hand hand, boolean fromServerPlayer, CallbackInfo info) {
+        if ((!mob.handSwinging || mob.handSwingTicks >= getHandSwingDuration() / 2 || mob.handSwingTicks < 0) && mob.world instanceof ServerWorld) {
+            HitResult result = mob.raycast(5.5, 0, false);
+            if (result.getType() != HitResult.Type.BLOCK) {
+                ItemStack stack = mob.getStackInHand(hand);
+                if (ItemSoundModifications.SWINGING_MAP.containsKey(stack.getItem())) {
+                    SoundEvent sound = ItemSoundModifications.SWINGING_MAP.get(stack.getItem()).get(stack);
+                    mob.getWorld().playSound(null, mob.getX(), mob.getY(), mob.getZ(), sound, mob.getSoundCategory(), 1, 1);
+                }
             }
         }
     }
