52094b76c592223c7f8f60dd5a661ce6b4378f55
buntennn <bunten2005@gmail.com>
2022-09-10 11:03:35 -0500

    make mixin less

--- src/main/java/com/ninni/decibel/mixin/EntityMixin.java
+++ src/main/java/com/ninni/decibel/mixin/EntityMixin.java
@@ -1,17 +1,16 @@
 package com.ninni.decibel.mixin;
 
-import com.ninni.decibel.sound.ArmorSoundModifications;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.ninni.decibel.sound.ArmorSoundModifications;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.util.math.BlockPos;
 
 @Mixin(Entity.class)
 public class EntityMixin {
@@ -19,15 +18,10 @@ public class EntityMixin {
     @Inject(at = @At("HEAD"), method = "playStepSound")
     public void modifyStepSound(BlockPos pos, BlockState state, CallbackInfo ci) {
         Entity entity = (Entity) (Object) this;
-        Iterable<ItemStack> iterable = entity.getArmorItems();
-
-        for (ItemStack itemStack : iterable) {
-            Item item = itemStack.getItem();
-            for (ArmorMaterial armorMaterial : ArmorSoundModifications.SOUND_MAP.keySet()) {
-                if ((item instanceof ArmorItem armorItem) && armorMaterial == armorItem.getMaterial()) {
-                    entity.playSound(ArmorSoundModifications.SOUND_MAP.get(armorMaterial).get(armorMaterial), 1, 1);
-                }
-            }
-        }
+        entity.getArmorItems().forEach((stack) -> {
+            if (stack.getItem() instanceof ArmorItem item && ArmorSoundModifications.SOUND_MAP.containsKey(item.getMaterial())) {
+                entity.playSound(ArmorSoundModifications.SOUND_MAP.get(item.getMaterial()).get(item.getMaterial()), 1, 1);
+            } 
+        });
     }
 }
