ff6dd03718bdc7bdcf998006059b1761972c4c33
buntennn <bunten2005@gmail.com>
2022-12-05 01:15:19 -0600

    add armor step sound volume config

--- src/main/java/com/ninni/decibel/config/DecibelConfig.java
+++ src/main/java/com/ninni/decibel/config/DecibelConfig.java
@@ -1,12 +1,16 @@
 package com.ninni.decibel.config;
 
+import com.ninni.decibel.Decibel;
+
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 
-@Config(name = "decibel")
+@Config(name = Decibel.MOD_ID)
 public class DecibelConfig implements ConfigData {
 
     public boolean addArmorStepSounds = true;
+    public float armorStepVolume = 0.5F;
+
     public boolean addBowReadySound = true;
     public boolean addItemUseSounds = true;
     public boolean addWeaponSwingSounds = true;
--- src/main/java/com/ninni/decibel/mixin/EntityMixin.java
+++ src/main/java/com/ninni/decibel/mixin/EntityMixin.java
@@ -1,6 +1,7 @@
 package com.ninni.decibel.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,17 +13,21 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.Shapes;
 
 @Mixin(Entity.class)
 public class EntityMixin {
 
+    @Unique
+    private final Entity entity = (Entity) (Object) this;
+
     @Inject(at = @At("HEAD"), method = "playStepSound")
     public void D$playStepSound(BlockPos pos, BlockState state, CallbackInfo ci) {
-        Entity entity = (Entity) (Object) this;
         entity.getArmorSlots().forEach((stack) -> {
-            if (!Decibel.getConfig().addArmorStepSounds) return;
+            if (Decibel.getConfig().addArmorStepSounds)
+            if (state.getCollisionShape(entity.getLevel(), entity.blockPosition()) != Shapes.empty())
             if (stack.getItem() instanceof ArmorItem item && ArmorSoundModifications.SOUND_MAP.containsKey(item.getMaterial())) {
-                entity.playSound(ArmorSoundModifications.SOUND_MAP.get(item.getMaterial()), 1, 1);
+                entity.playSound(ArmorSoundModifications.SOUND_MAP.get(item.getMaterial()), Decibel.getConfig().armorStepVolume, 1);
             } 
         });
     }
