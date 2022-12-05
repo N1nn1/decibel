886b5e8e4515de395e38b4e7defdcea78b84b3f2
buntennn <bunten2005@gmail.com>
2022-12-05 01:24:50 -0600

    make class thing so it easier

--- src/main/java/com/ninni/decibel/sound/DecibelSoundTypes.java
+++ src/main/java/com/ninni/decibel/sound/DecibelSoundTypes.java
@@ -1,28 +1,39 @@
 package com.ninni.decibel.sound;
 
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.level.block.SoundType;
 
 public class DecibelSoundTypes {
 
-    public static final SoundType CACTUS = register("cactus");
-    public static final SoundType CLAY = register("clay");
-    public static final SoundType COARSE_DIRT = register("coarse_dirt");
-    public static final SoundType PODZOL = register("podzol");
-    public static final SoundType DIRT = register("dirt");
-    public static final SoundType FARMLAND = register("farmland");
-    public static final SoundType GENERIC_LEAVES = register("generic_leaves");
-    public static final SoundType SAPLING = register("sapling");
-    public static final SoundType GLASS = register("glass");
-    public static final SoundType GRASS_BLOCK = register("grass_block");
-    public static final SoundType SMALL_PLANT = register("small_plant");
-    public static final SoundType LARGE_PLANT = register("large_plant");
-    public static final SoundType COBWEB = register("cobweb");
-    public static final SoundType GRAVEL = register("gravel");
-    public static final SoundType METAL = register("metal");
-    public static final SoundType WET_FARMLAND = register("wet_farmland");
-    public static final SoundType SAND = register("sand");
+    public static final SoundType CACTUS = Type.of("cactus");
+    public static final SoundType CLAY = Type.of("clay");
+    public static final SoundType COARSE_DIRT = Type.of("coarse_dirt");
+    public static final SoundType PODZOL = Type.of("podzol");
+    public static final SoundType DIRT = Type.of("dirt");
+    public static final SoundType FARMLAND = Type.of("farmland");
+    public static final SoundType GENERIC_LEAVES = Type.of("generic_leaves");
+    public static final SoundType SAPLING = Type.of("sapling");
+    public static final SoundType GLASS = Type.of("glass");
+    public static final SoundType GRASS_BLOCK = Type.of("grass_block");
+    public static final SoundType SMALL_PLANT = Type.of("small_plant");
+    public static final SoundType LARGE_PLANT = Type.of("large_plant");
+    public static final SoundType COBWEB = Type.of("cobweb");
+    public static final SoundType GRAVEL = Type.of("gravel");
+    public static final SoundType METAL = Type.of("metal");
+    public static final SoundType WET_FARMLAND = Type.of("wet_farmland");
+    public static final SoundType SAND = Type.of("sand");
 
-    private static SoundType register(String name) {
-        return new SoundType(1, 1, DecibelSounds.register("block." + name + ".break"), DecibelSounds.register("block." + name + ".step"), DecibelSounds.register("block." + name + ".place"), DecibelSounds.register("block." + name + ".hit"), DecibelSounds.register("block." + name + ".fall"));
+    private static class Type extends SoundType {
+        private Type(String name) {
+            super(1, 1, register(name, "break"), register(name, "step"), register(name, "place"), register(name, "hit"), register(name, "fall"));
+        }
+    
+        private static SoundEvent register(String name, String type) {
+            return DecibelSounds.register("block." + name + "." + type);
+        }
+
+        public static Type of(String name) {
+            return new Type(name);
+        }
     }
 }
